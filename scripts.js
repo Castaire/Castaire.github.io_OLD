ce60f9e7ebc6ff66525830859f4d94aed5677ec8
Castaire <iv.castaire@gmail.com>
2019-03-01 18:33:19 -0800

    MVP completed; will add pages for work descriptions in separate dev branch

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	console.log("Thanks for visiting my site!");
 
 	// set up isotope grid
 	$grid = setupGrid();
@@ -8,23 +9,28 @@ $(document).ready(function(){
 		sortAscending: false
 	});
 
-	// show ONLY 'project' items, by default
+	// references to checkboxes
 	$projectInput = $("#board #filterbar .customCheckBox #showProjects");
-	$projectLabel = $projectInput.next(); 
-	$projectInput.prop('checked', true);
-	$projectLabel.css('background-color', $projectLabel.css('border-color'));
+	$workInput = $("#board #filterbar .customCheckBox #showWork");
+	$byNameInput = $("#board #filterbar .customCheckBox #byName");
+	$byDateInput = $("#board #filterbar .customCheckBox #byDate");
 
-	$grid.isotope({filter: ".project"});
+	// setup checkbox styles
+	initCheckboxStates($projectInput, $workInput, $byNameInput, $byDateInput);
 	
+	// add handler to filtering options (work / project)
+	$("#board #filterbar .customCheckBox > #showProjects,#showWork").click(
+		{grid: $grid, projectElem : $projectInput, workElem : $workInput}, 
+		handleFiltering);
 
-	// add handlers to sorting / filtering options
-	$("#board #filterbar .customCheckBox > label").click(
-		{grid: $grid}, handleCheckBoxClick
-	);
+	// add handler to sorting options (by date / by name)
+	$("#board #filterbar .customCheckBox > #byDate,#byName").click(
+		{grid: $grid, byNameElem : $byNameInput, byDateElem : $byDateInput }, 
+		handleSorting);
 
 });
 
-
+// USAGE: 	initializes grid and sort-by keys
 function setupGrid(){
 	var $grid = $('#grid').isotope({
 		itemSelector: '.note-item',
@@ -46,28 +52,84 @@ function setupGrid(){
 }
 
 
+/**
+ *  USAGE: initializes checkboxes to the states described below
+ * 	STATES: 
+ *		- project, byDate 	:  checked, filled-in
+ *     	- work, byName 		:  unchcked, not filled-in
+ */
+function initCheckboxStates(projectInput, workInput, byDateInput, byNameInput){	
+	$projectLabel = $projectInput.next();
+	$projectLabel.css('background-color', $projectLabel.css('border-color'));
+	$projectInput.prop("checked", "true");
 
-function handleCheckBoxClick(event){
-	var $g = $(event.data.grid); 				// isotope grid
-	var $checkboxLabel = $(event.target);		// get label for the selected checkbox
-	var $input = $($checkboxLabel.prev()); 		// get associated 'input'
+	$byDateLabel = $byDateInput.next();
+	$byDateLabel.css('background-color', $byDateLabel.css('border-color'));
+	$byDateInput.prop("checked", "true");
 
-	var type = $checkboxLabel.attr('class');
-
-	var filterString = '';
-	var colorString = '';
-
-	if($input.is(':checked')){
-		filterString = {val: `:not(.${type})`};
-		colorString= {val: "initial"};
-	}else{
-		filterString = {val: `.${type}`};
-		colorString = {val: $checkboxLabel.css("border-color")};
-	}
-
-	$g.isotope({ filter: `${filterString.val}`});
-	$checkboxLabel.css("background-color", `${colorString.val}`);
+	// show ONLY 'project' items, by date (ascending)
+	$grid.isotope({filter: '.project'});
+	$grid.isotope({sortBy: 'date'});
 }
 
+
+// USAGE: 	displays projects and/or work tiles, depending on user input
+function handleFiltering(event){
+	var wasProjectChecked = $(event.data.projectElem).is(":checked");
+	var wasWorkChecked = $(event.data.workElem).is(":checked");
+
+	// update grid cells
+	var filterString = '';
+	if(wasProjectChecked){
+		wasWorkChecked? filterString = {val: "*"} : filterString = {val: ".project"};
+	}else{
+		wasWorkChecked? filterString = {val: ".work"} : filterString = {val: ":not(.work,.project)"};
+	}
+	$(event.data.grid).isotope({filter: filterString.val});
+
+	// update checkbox fill
+	updateCheckBox($(event.target));
+}
+
+
+// USAGE: at any time, grid tiles must be sorted by EITHER "name" OR "date"
+function handleSorting(event){
+	var $g = $(event.data.grid);
+	var $filter = $(event.target);
+
+	if($filter.attr("id") == "byDate"){
+		if($filter.is(":checked")){
+			$g.isotope({sortBy: 'date', sortAscending: false});
+			$(event.data.byNameElem).prop("checked", false);
+		}else{
+			$g.isotope({sortBy: 'name', sortAscending: true});
+			$(event.data.byNameElem).prop("checked", true);
+		}
+
+	}else{
+		if($filter.is(":checked")){
+			$g.isotope({sortBy: 'name', sortAscending: true});
+			$(event.data.byDateElem).prop("checked", false);
+		}else{
+			$g.isotope({sortBy: 'date', sortAscending: false});
+			$(event.data.byDateElem).prop("checked", true);
+		}
+
+	}
+
+	updateCheckBox($(event.data.byNameElem));
+	updateCheckBox($(event.data.byDateElem));
+}
+
+// USAGE: 	updates check box fill depending on state
+function updateCheckBox(checkboxInput){
+	var $label = $(checkboxInput).next();
+
+	if($(checkboxInput).is(":checked")){
+		$label.css("background-color", $label.css("border-color"));
+	}else{
+		$label.css("background-color", "initial");
+	}
+}
 
 
